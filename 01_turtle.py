3bfa04381f5d94b3a85f540920ad5494f207303c
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-01 16:08:16 +0900

    Update 01_turtle.py

--- 01_turtle.py
+++ 01_turtle.py
@@ -1,4 +1,26 @@
 import turtle as t
-t.shape("turtle")
+t.shape('turtle')
+
+# 삼각형 그리기
+t.color("red")
+t.forward(300)
+t.left(120)
+t.forward(300)
+t.left(120)
+t.forward(300)
+t.left(120)
+
+# 사각형 그리기
+t.color("blue")
+t.forward(300)
 t.right(90)
-t.forward(50)
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+
+# 원 그리기
+t.color("green")
+t.circle(150)
